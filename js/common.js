f514510b0ee3f7a10dfa2f36c004b1422260d368
jiang.feng <jiang.feng@huifu.com>
2020-05-11 13:32:37 +0800

    update

--- js/common.js
+++ js/common.js
@@ -1,15 +1,15 @@
 const settings = require('electron-settings');
 const request = require("axios");
 
-request.defaults.headers = {
-	"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
-}
+request.defaults.headers.post['Content-Type'] = 'application/json';
+request.defaults.headers["Access-Control-Allow-Origin"] = "*";
 
 async function heartbeat() {
 	let ctlUrl = await settings.get('User.CtlUrl');
 	console.info(ctlUrl);
 	if (!ctlUrl || ctlUrl === 'null') return '';
-	return await requsetCtl(ctlUrl + '/rebotapi/mlist');
+	let res =  await requsetCtl(ctlUrl + '/rebotapi/mlist');
+	return res && res.data ? res.data : '';
 }
 
 async function requsetCtl(sCtlUrl) {
